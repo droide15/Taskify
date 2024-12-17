5c37f7161b19e4400fe1c9137f2c62369748047c
jorge alberto nuñez cuevas <nuniez.jorge@gmail.com>
2024-12-17 07:49:05 -0800

    add unit tests

--- src/test/java/com/itjuana/taskifyapi/TaskifyapiApplicationTests.java
+++ src/test/java/com/itjuana/taskifyapi/TaskifyapiApplicationTests.java
@@ -5,9 +5,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskifyapiApplicationTests {
+	@Mock
+	TaskService service;
+
+	@Rule public MockitoRule rule = MockitoJUnit.rule();
 
 	@Test
-	void contextLoads() {
+	public void testGetAllTasks() {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(1, "Prepare infraestructure", "Prepare the infraestructure", "Completed"));
+		tasks.add(new Task(2, "Install equipement", "Install the equipement", "Pending"));
+
+		when(service.getAllTasks()).thenReturn(tasks);
+		assertArrayEquals(tasks, service.getAllTasks());
+		assertEquals(task, service.getTask(1));
 	}
 
+	@Test
+	public void testGetTask() {
+		Task task = new Task(1, "Prepare infraestructure", "Prepare the infraestructure", "Completed");
+
+		when(service.getTask()).thenReturn(task);
+		assertEquals(task, service.getTask(1));
+	}
 }
