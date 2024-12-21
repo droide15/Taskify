d97b614f847f5956ee0cf2981dd1b7f9732e3fb7
jorge <nuniez.jorge@gmail.com>
2024-12-21 12:50:09 -0800

    fix issue with lombok and bug in the function to delete tasks

--- src/main/java/com/itjuana/taskifyapi/model/Task.java
+++ src/main/java/com/itjuana/taskifyapi/model/Task.java
@@ -19,46 +19,4 @@ public class Task {
     private String description;
     @NotNull
     private String status;
-
-    public Task() {
-    }
-
-    public Task(int id, String title, String description, String status) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
